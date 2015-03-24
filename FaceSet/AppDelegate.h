ff13f65a18d98765d145eec9e334978cbe9466a0
Janardan Yri <janardan.yri@me.com>
2015-03-23 19:35:55 -0700

    Initial Commit

---
+++ FaceSet/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FaceSet
+//
+//  Created by Wren on 3/23/15.
+//  Copyright (c) 2015 Janardan Yri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FaceSet/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FaceSet
+//
+//  Created by Wren on 3/23/15.
+//  Copyright (c) 2015 Janardan Yri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
