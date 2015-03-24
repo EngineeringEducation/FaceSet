47d57d648db581550a3abaa7d458724a3fce7d33
Janardan Yri <janardan.yri@me.com>
2015-03-23 19:44:04 -0700

    Take out storyboard, modest jiggerment.

--- FaceSet/ViewController.m
+++ FaceSet/ViewController.m
@@ -8,20 +8,32 @@
 
 #import "ViewController.h"
 
+
 @interface ViewController ()
+
+@property (nonatomic) CIDetector *faceDetector;
 
 @end
 
+
 @implementation ViewController
+
+- (instancetype)init {
+  return [self initWithCollectionViewLayout:[[UICollectionViewFlowLayout alloc] init]];
+}
 
 - (void)viewDidLoad {
   [super viewDidLoad];
+
+  self.faceDetector = [CIDetector detectorOfType:CIDetectorTypeFace context:nil options:nil];
   // Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning {
-  [super didReceiveMemoryWarning];
-  // Dispose of any resources that can be recreated.
+- (void)viewDidAppear:(BOOL)animated {
+  [super viewDidAppear:animated];
+
+
+
 }
 
 @end
